4f7f45fe04d4adf5cfffc48db0fe63b13431d7ce
Anderson Zhou <andersonzhouca@gmail.com>
2026-01-26 03:43:41 -0800

    removed unused components

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,20 +1,16 @@
-import { LoginGradient } from '@/components/gradients/login-gradient';
 import '../../styles/login.css';
-import { LoginCardGradient } from '@/components/gradients/login-card-gradient';
 import { LoginForm } from '@/components/authentication/login-form';
 import { GhLoginButton } from '@/components/authentication/gh-login-button';
 
 export default function LoginPage() {
   return (
     <div>
-      <LoginGradient />
       <div className={'flex flex-col'}>
         <div
           className={
             'mx-auto mt-[112px] bg-background/80 w-[343px] md:w-[488px] gap-5 flex-col rounded-lg rounded-b-none login-card-border backdrop-blur-[6px]'
           }
         >
-          <LoginCardGradient />
           <LoginForm />
         </div>
         <GhLoginButton label={'Log in with GitHub'} />
--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -1,20 +1,16 @@
-import { LoginGradient } from '@/components/gradients/login-gradient';
 import '../../styles/login.css';
-import { LoginCardGradient } from '@/components/gradients/login-card-gradient';
 import { GhLoginButton } from '@/components/authentication/gh-login-button';
 import { SignupForm } from '@/components/authentication/sign-up-form';
 
 export default function SignupPage() {
   return (
     <div>
-      <LoginGradient />
       <div className={'flex flex-col'}>
         <div
           className={
             'mx-auto mt-[112px] bg-background/80 w-[343px] md:w-[488px] gap-5 flex-col rounded-lg rounded-b-none login-card-border backdrop-blur-[6px]'
           }
         >
-          <LoginCardGradient />
           <SignupForm />
         </div>
         <GhLoginButton label={'Sign up with GitHub'} />
