03ecad0fe8da065bdae91f8e54fc1a02adbe58aa
plmmzoe <zoeyueshuangzhang@gmail.com>
2026-02-03 23:59:40 -0800

    Update logo and theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,15 @@ export const metadata: Metadata = {
   title: 'AeroEdit',
   description:
     'AeroEdit is a powerful team design collaboration app and image editor. With plans for businesses of all sizes, streamline your workflow with real-time collaboration, advanced editing tools, and seamless project management.',
+  icons: {
+    icon: [
+      {
+        url: '/assets/logo/logo.png',
+        sizes: '32x32',
+        type: 'image/png',
+      },
+    ],
+  },
 };
 
 export default function RootLayout({
@@ -18,7 +27,7 @@ export default function RootLayout({
   children: ReactNode;
 }>) {
   return (
-    <html lang="en" className={'min-h-full dark'}>
+    <html lang="en" className="min-h-full">
       <body>
         {children}
         <Toaster />
